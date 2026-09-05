7230fc4c29acbc68c4de5e59f9ff69b25365e62b
JUNAID S <166510560+JUN41DS2709@users.noreply.github.com>
2026-09-05 19:40:12 +0530

    Implement basic client for socket communication

---
+++ client.py
@@ -0,0 +1,30 @@
+import socket
+import threading
+
+def send_msg():
+    while True:
+        msg = input().encode()
+        s.send(msg)
+
+def recv_msg():
+    while True:
+        recv = s.recv(1024)
+        print(recv.decode())
+
+
+s = socket.socket(
+    socket.AF_INET,
+    socket.SOCK_STREAM
+)
+print("Connecting...")
+while True:
+    try:
+        s.connect(("127.0.0.1" , 8888))
+        break
+    except ConnectionRefusedError:
+        continue
+print("Connected")
+
+t1 = threading.Thread(target=send_msg)
+t1.start()
+recv_msg()
